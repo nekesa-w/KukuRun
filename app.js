1cf9bbe09f59ffa8076ac35469b055a9a490bf8e
Jesse <yourmail@mail.com>
2023-11-22 11:49:25 +0300

    Trial Run

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// Request html canvas element 
-var canvas = document.getElementById("canvas"); 
-  
-// Create a WebGL rendering context   
-var gl = canvas.getContext("webgl2"); 
-  
-// Tell user if their browser does not support WebGL 
-if (!gl) { 
-    alert("Your browser does not support WebGL"); 
-} 
-  
-// Set the color of the canvas. 
-// Parameters are RGB colors (red, green, blue, alpha) 
-gl.clearColor(0, 0.6, 0.0, 1.0); 
-// Clear the color buffer with specified color 
-gl.clear(gl.COLOR_BUFFER_BIT);
+var chicken = document.getElementById('chicken');
+var obstacle = document.getElementById('obstacle');
+var counter = 0;
+
+function jump(){
+if(chicken.classList != 'animate'){
+    chicken.classList.add('animate');
+}
+setTimeout(function(){
+    chicken.classList.remove('animate');
+    counter++;
+    },500);
+}
+
+var lose = setInterval(function(){
+    var chickenTop = parseInt(window.getComputedStyle(chicken).getPropertyValue('top'));
+    var blockLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+
+    if (blockLeft < 20 && blockLeft > 0 && chickenTop >= 130) {
+        obstacle.style.animation = "none";
+        obstacle.style.display = "none";
+        alert("SCORE: " + counter);  // Concatenate "SCORE:" with the counter
+        counter = 0;
+    }
+},10);
